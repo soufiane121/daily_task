2e3357b6930b78697f758098411d2e1516896e49
soufiane121 <soufiane.oucherrou@gmail.com>
2020-03-23 11:45:07 -0400

    fix bug of sign in

--- daily_task_backend/config/routes.rb
+++ daily_task_backend/config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   post "/login", to: "auth#login"
   get "/owner_auto_login", to: "auth#auto_login"
   post '/user_login', to: 'authi#login'
+  get "/user_auto_login", to: "authi#auto_login"
+
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
