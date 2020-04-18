8ea0d945eea8b72791e04f29e072123b6d3c2aa8
soufiane121 <soufiane.oucherrou@gmail.com>
2020-04-18 16:32:51 -0400

    status full working

--- daily_task_frontend/Components/ScrollList.js
+++ daily_task_frontend/Components/ScrollList.js
@@ -1,39 +1,38 @@
 import React from 'react';
 import { Text, View, StyleSheet, SafeAreaView, ScrollView, FlatList } from 'react-native';
+import * as Animatable from 'react-native-animatable';
+import Status from '../IngredientTasks/Status'
 
 const ScrollList = () => {
 
     return (
-        <ScrollView style={styles.scrollView} horizontal={true} scrollEnabled={true} showsHorizontalScrollIndicator={false}>
-            <Text style={styles.text}>hello</Text>
-            <Text style={styles.text}>hey</Text>
-            <Text style={styles.text}>hola</Text>
-            <Text style={styles.text}>salut</Text>
-            <Text style={styles.text}>marhaba</Text>
-            <Text style={styles.text}>voila</Text>
-            <Text style={styles.text}>horizontal</Text>
-            <Text style={styles.text}>horizontal</Text>
-            <Text style={styles.text}>horizontal</Text>
-            <Text style={styles.text}>horizontal</Text>
-            <Text style={styles.text}>horizontal</Text>
-            <Text style={styles.text}>horizontal</Text>
-            <Text style={styles.text}>horizontal</Text>
-            <Text style={styles.text}>horizontal</Text>
-            <Text style={styles.text}>horizontal</Text>
-        </ScrollView>
+        <Animatable.View animation={'slideInRight'} >
+            <ScrollView style={styles.scrollView} horizontal={true} scrollEnabled={true} showsHorizontalScrollIndicator={false}>
+                <Status />
+                <Text style={styles.text}>hey</Text>
+                <Text style={styles.text}>hola</Text>
+                <Text style={styles.text}>salut</Text>
+                <Text style={styles.text}>marhaba</Text>
+                <Text style={styles.text}>voila</Text>
+                <Text style={styles.text}>horizontal</Text>
+                <Text style={styles.text}>horizontal</Text>
+                <Text style={styles.text}>horizontal</Text>
+            </ScrollView>
+        </Animatable.View>
     )
 };
 
 const styles = StyleSheet.create({
 
     scrollView: {
-        backgroundColor: '#dee3e2',
         marginVertical: 3,
-        marginHorizontal: 1,
+        marginHorizontal: 3,
     },
     text: {
         fontSize: 17,
-       margin: 3
+        margin: 1,
+        backgroundColor: '#dee3e2',
+        width: 95
     },
 });
 export default ScrollList;
---
+++ daily_task_frontend/IngredientTasks/Status.js
@@ -0,0 +1,150 @@
+import React, { useState } from 'react';
+import { Text, View, StyleSheet, TouchableWithoutFeedback } from 'react-native';
+import { Tooltip } from 'react-native-elements'
+
+const Status = () => {
+    const [working, setWorking] = useState(false)
+    const [stucking, setStucking] = useState(false)
+    const [done, setDone] = useState(false)
+
+
+    const handleWorking = () => {
+        setWorking(true)
+        setStucking(false)
+        setDone(false)
+    }
+
+    const handleStucking = () => {
+        setStucking(true)
+        setWorking(false)
+        setDone(false)
+
+    }
+
+    const handleDone = () => {
+        setDone(true)
+        setWorking(false)
+        setStucking(false)
+    }
+
+    const handlSingle = () => {
+        if (!stucking && !working && !done) {
+            return <Text style={styles.txt}>Status</Text>
+        } else if (stucking) {
+            return <Text style={styles.txtStuck}>Stuck</Text>
+        } else if (working && !stucking) {
+            return <Text style={styles.txtWork}>Working on it</Text>
+        } else if (done && !stucking && !working) {
+            return <Text style={styles.txtDone}>Done</Text>
+        }
+    }
+
+    const StatusChoose = () => {
+        return (
+            <View style={{ flex: 1 }}>
+                <TouchableWithoutFeedback onPress={handleWorking}>
+                    <Text style={styles.working}>Working on it</Text>
+                </TouchableWithoutFeedback>
+                <TouchableWithoutFeedback onPress={handleStucking}>
+                    <Text style={styles.stuck}>Stuck</Text>
+                </TouchableWithoutFeedback>
+                <TouchableWithoutFeedback onPress={handleDone}>
+                    <Text style={styles.done}>Done</Text>
+                </TouchableWithoutFeedback>
+            </View>
+        )
+    }
+
+    return (
+        <View style={styles.container} >
+            <Tooltip
+                withPointer={true}
+                popover={<StatusChoose />}
+                width={200}
+                height={140}
+                containerStyle={styles.toptip}
+                pointerColor='black'
+                overlayColor="transparent"
+            >
+                {handlSingle()}
+            </Tooltip>
+        </View>
+    )
+};
+
+const styles = StyleSheet.create({
+    container: {
+        fontSize: 17,
+        margin: 1,
+        backgroundColor: '#dee3e2',
+        width: 95,
+    },
+    txt: {
+        alignSelf: 'center',
+        marginTop: 11,
+        color: 'grey',
+        fontWeight: '500'
+    },
+    toptip: {
+        backgroundColor: '#f2f2f2',
+    },
+    working: {
+        width: 190,
+        height: 40,
+        backgroundColor: '#f8a978',
+        paddingVertical: 8,
+        paddingLeft: 35,
+        color: '#ecfcff',
+        fontWeight: '500',
+        fontSize: 19,
+        marginBottom: 2
+    },
+    txtWork: {
+        backgroundColor: '#f8a978',
+        height: 44,
+        paddingVertical: 10,
+        fontWeight: '600',
+        fontSize: 15,
+        color: '#ecfcff',
+        paddingHorizontal: 1
+    },
+    stuck: {
+        backgroundColor: '#c70039',
+        height: 40,
+        color: '#ecfcff',
+        fontSize: 17,
+        fontWeight: '600',
+        paddingHorizontal: 70,
+        paddingVertical: 9,
+        marginBottom: 2
+    },
+    txtStuck: {
+        backgroundColor: '#c70039',
+        height: 40,
+        color: '#ecfcff',
+        fontSize: 17,
+        fontWeight: '600',
+        paddingVertical: 9,
+        paddingHorizontal: 19
+    },
+    done: {
+        height: 40,
+        backgroundColor: 'green',
+        color: '#ecfcff',
+        fontSize: 17,
+        fontWeight: '600',
+        paddingHorizontal: 70,
+        paddingVertical: 9,
+        marginBottom: 2
+    },
+    txtDone:{
+        backgroundColor: 'green',
+        height: 40,
+        color: '#ecfcff',
+        fontSize: 17,
+        fontWeight: '600',
+        paddingVertical: 9,
+        paddingHorizontal: 22
+    }
+});
+export default Status;
