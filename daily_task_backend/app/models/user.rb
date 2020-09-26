c06512a8ee10d649566cf5424a7670273e0d3384
soufiane121 <soufiane.oucherrou@gmail.com>
2020-09-26 14:03:03 -0400

    user fixed

--- daily_task_backend/app/models/user.rb
+++ daily_task_backend/app/models/user.rb
@@ -2,7 +2,6 @@ class User < ApplicationRecord
     belongs_to :owner
     has_many :messages
     has_many :chats, through: :messages
-    # attribute :variables, :variable_array, default: []
     validates :first_name, :last_name, :email, presence: true
     validates :email, uniqueness: true
     validates :password_digest, length: { minimum: 2}
